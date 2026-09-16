6f32b65210e736a6fd0a31b18c37dc77620964f6
K22028 <k22028kk@aitech.ac.jp>
2026-09-16 20:22:54 +0900

    阿部寛を追加
    阿部寛を表示する処理を追加

--- index.js
+++ index.js
@@ -1,4 +1,47 @@
-document.querySelector('.hamburger').addEventListener('click', function(){
-    this.classList.toggle('active');
-    document.querySelector('.slide-menu').classList.toggle('active');
-})
+const urls = [
+  "https://abehiroshi.la.coocan.jp/",
+  "https://abehiroshi.la.coocan.jp/",
+  "https://abehiroshi.la.coocan.jp/",
+  "https://abehiroshi.la.coocan.jp/",
+  "https://abehiroshi.la.coocan.jp/",
+  "https://abehiroshi.la.coocan.jp/",
+  "https://abehiroshi.la.coocan.jp/",
+  "https://abehiroshi.la.coocan.jp/",
+  "https://abehiroshi.la.coocan.jp/",
+  "https://abehiroshi.la.coocan.jp/"
+];
+
+// document.querySelector('.hamburger').addEventListener('click', function(){
+//     this.classList.toggle('active');
+//     document.querySelector('.slide-menu').classList.toggle('active');
+// })
+
+// ボタンの要素を取得
+const button = document.getElementById("extra_button");
+const button2 = document.getElementById("extra_button2");
+
+// ボタンがクリックされたらアラートを出す
+button.addEventListener("click", () => {
+     // 確認ダイアログを表示
+    const result = window.confirm('本当に閲覧しますか？');
+
+    // ユーザーの選択で分岐
+    if (result) {
+      const result = window.confirm('本当の本当に閲覧しますか？');
+      if(result){
+        const result = window.confirm('そんなにみたいの？');
+        if(result){
+            const result = window.confirm('じゃあ見せてあげます');
+            if(result){
+                button2.style.display = 'inline-block'; // 表示させる
+            }
+        }
+      }
+    }
+  });
+
+button2.addEventListener('click', () => {
+  urls.forEach(url => {
+                window.open(url, '_blank');
+                });
+});
